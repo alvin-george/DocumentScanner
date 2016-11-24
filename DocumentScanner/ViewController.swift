655aae0571ed9ac62bc8a6d65b6033c198acfdd4
Alvin George <alvingeorge@alvins-mac-mini.ftsindia.in>
2016-11-24 17:35:39 +0530

    Initial Commit

---
+++ DocumentScanner/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DocumentScanner
+//
+//  Created by Alvin George on 11/24/16.
+//  Copyright © 2016 Alvin George. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
